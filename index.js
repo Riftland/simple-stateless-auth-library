575e5940c8b9f9eb4ed1f94bc2ae3b090df11743
Francisco Molpeceres <franciscomoldepeceres@OCS-FMOLPECERES-MAC.local>
2023-04-28 20:17:50 +0200

    feat: added utils

---
+++ index.js
@@ -0,0 +1,24 @@
+const { hash, cookie, jwt } = require("./utils");
+
+const serialize = (res, payload) => {
+  const token = jwt.sign(payload);
+  cookie.create(res, token);
+};
+
+const deserialize = (req) => {
+  const { access_token } = req.cookies;
+
+  const payload = jwt.verify(access_token);
+
+  if (!payload) return false;
+
+  return payload;
+};
+
+module.exports = {
+  hash,
+  cookie,
+  jwt,
+  serialize,
+  deserialize,
+};
---
+++ utils/index.js
@@ -0,0 +1,9 @@
+const hash = require("./hash");
+const cookie = require("./cookie");
+const jwt = require("./jwt");
+
+module.exports = {
+  hash,
+  cookie,
+  jwt,
+};
